76180eed0d7f1ee2daed72c58d539a8317133c94
Ballard Ingram <ballardingram@outlook.com>
2022-03-06 15:51:51 -0600

    Adding questions and media to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,84 @@
-// List of questions.
+// Click to reveal button code.
+document.addEventListener("DOMContentLoaded", function(event){
+    showQuizBtn.addEventListener('click', function(event) {
+    console.log("what")
+    buildQuiz()
+    })
+})
 
-let questions = [
-    {
-        question: "What is the greatest city in the United States of America?",
-        choiceA: "Chicago, Illinois",
-        choiceB: "Austin, Texas",
-        choiceC: "Seattle, Washington",
-        correct: "A"
-    },{
-        question: "Assuming you know where the greatest city in the USA is located, what state is directly north of that?",
-        choiceA: "Nebraska",
-        choiceB: "Wisconsin",
-        choiceC: "Florida",
-        correct: "B"
-    },{
-        question: "Since its obvious you know where the greatest city in the US is located, what state is furthest west from there?",
-        choiceA: "Utah",
-        choiceB: "Idaho",
-        choiceC: "Oregon",
-        correct: "C"
+function buildQuiz() {
+console.log('hey')
+const output = [];
+
+myQuestions.forEach((currentQuestion, questionNumber) => {
+  const answers = [];
+
+  for (var letter in currentQuestion.answers) {
+    answers.push(
+      `<label>
+      <input type="radio" name="question${questionNumber}" value="${letter}">
+      ${letter} :
+      ${currentQuestion.answers[letter]}
+    </label>`
+    );
+  }
+
+  output.push(
+    `<div class="question"> ${currentQuestion.question} </div>
+  <div class="answers"> ${answers.join("")} </div>`
+  );
+});
+
+quizContainer.innerHTML = output.join("");
+}
+
+
+const quizContainer = document.getElementById("quiz");
+const showQuizBtn = document.getElementById('showQuiz')
+const myQuestions = [{
+  question: "What state is known as the Lone Star State?",
+  answers: {
+    a: "Nebraska",
+    b: "Montana",
+    c: "Texas"
+  },
+  correctAnswer: "c"
+},
+{
+  question: "What state is home to 'Sin City'?",
+  answers: {
+    a: "Washington",
+    b: "Nevada",
+    c: "Florida"
+  },
+  correctAnswer: "b"
+},
+{
+    question: "What state is the largest?",
+    answers: {
+        a: "Alaska",
+        b: "Rhode Island",
+        c: "Hawaii"
+    },
+    correctAnswer:"a"
+}
+]
+
+var i = 0;
+function move() {
+    if (i ==0) {
+        i=1;
+        var elem = document.getElementById("progressbar");
+        var width = 1;
+        var id = setInterval(frame, 10);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i=0;
+            } else {
+                width++;
+                elem.style.width + "%";
+            }
+        }
     }
-];
-
-const lastQuestion = questions.length - 1;
-let ruinningQuestion = 0;
-
-function renderQuestion() {
-    let q = questions[runningQuestion];
-    question.innerHTML="<p>" + q.question + "</p>"
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
 }
