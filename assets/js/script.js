d747f6043948fd360e9ca811f6868e99020ab9ce
Ballard Ingram <ballardingram@outlook.com>
2022-03-08 13:46:31 -0600

    updated css and js files with new code

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById ('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener("click", startGame)
+
+function startGame () {
+startButton.classList.add('hide')
+shuffledQuestions = questions.sort(() => Math.random() - .5)
+currentQuestionIndex = 0
+questionContainerElement.classList.remove('hide')
+setNextQuestion()
+}
+
+function setNextQuestion () {
+  resetState()
+showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+questionElement.innerText = question.question
+question.answers.forEach(answer => {
+  const button = document.createElement('button')
+  button.innerText = answer.text
+  button.classList.add('btn')
+  if (answer.correct) {
+    button.dataset.correct = answer.correct
+  }
+  button.addEventListener('click', selectAnswer)
+  answerButtonsElement.appendChild(button)
+})
+}
+
+function resetState() {
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild
+    (answerButtonsElement.firstChild)
+
+  }
+}
+
+function selectAnswer () {
+
+}
+
+const questions = [
+  {
+    question: 'What year did Galaga come out?',
+    answers: [
+      {text: '1980', correct: false},
+      {text: '1981', correct: true},
+      {text: '1989', correct: false},
+      {text: '2001', correct: false}
+    ]
+  }
+]
